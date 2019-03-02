25aba07a1bdde8d2cd171c07aa7f19fd0a088c1a
Nick Cipollo <njc1115@gmail.com>
2019-03-02 17:34:58 -0500

    Migrate to android x.

--- app/src/main/java/fnew/fnew/HomeActivity.kt
+++ app/src/main/java/fnew/fnew/HomeActivity.kt
@@ -1,6 +1,6 @@
 package fnew.fnew
 
-import android.support.v7.app.AppCompatActivity
+import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
 class HomeActivity : AppCompatActivity() {
